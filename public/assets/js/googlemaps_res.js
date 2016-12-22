3f866fb8430a11fae007d666a94e108c26a000b7
zitim <s911297@gmail.com>
2016-12-22 16:37:48 +0800

    add filter opening hour

--- public/assets/js/googlemaps_res.js
+++ public/assets/js/googlemaps_res.js
@@ -8,11 +8,11 @@ var focusList;
 var infoWindows=[];
 var userPosition = { lat: 23.973875, lng: 120.982024 };
 var dt = new Date();
-  var date=new Date();
-  var day=(date.getDay());
-  var hour=(date.getHours());
-  var minute=(date.getMinutes());
-  var now=(hour*60)+minute;
+var date=new Date();
+var day=date.getDay();
+var hour=date.getHours();
+var minute=date.getMinutes();
+var nowtime=(hour*60)+minute;
   
 
   jQuery(document).ready(function($) {
@@ -26,6 +26,8 @@ var dt = new Date();
       center: {lat: 22.999533, lng: 120.203401}
     });
 
+    $('#filter').append();
+
     geoFindMe();
     //includeData();
   }
@@ -33,7 +35,7 @@ var dt = new Date();
   function geoFindMe() {
     if (!navigator.geolocation) {
         alert("很抱歉，您的瀏覽器不支援定位服務");
-        includeData();
+        append_select(1);
         return;
     }
 
@@ -45,14 +47,26 @@ var dt = new Date();
             lat: userLat,
             lng: userLng
         };
-        includeData();
+        append_select(1);
     };
 
     function error() {
         //alert("已取消定位功能");
-        includeData();
+        append_select(1);
     };
     navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+  function append_select(i){
+    var i = i.toString();
+    document.getElementById('filter').innerHTML = "";
+    $('#filter').append(
+      '營業時間：<select id="opening_hours'+i+'">'+
+      '<option value="all" selected="selected">不限</option>'+
+      '<option value="opening">現在營業</option>'+
+      '</select>'
+    );
+    includeData();
   }
 
   function includeData() {
@@ -60,57 +74,62 @@ var dt = new Date();
       //console.log(data[293].餐飲店家名稱);
       //console.log(data[293].favorite);
       show_Data(data);
-      $('#opening_hours').change(function(){
+      $('#opening_hours1').change(function(){
 
-        if ( $('#opening_hours').val() == 'all' ) {
+        if ( $('#opening_hours1').val() == 'all' ) {
+            //console.log(12);
             deleteMarkers();
             document.getElementById('sidebar-left').innerHTML = "";
             show_Data(data);
-        }else if($('#opening_hours').val() == 'opening'){
+        }else if($('#opening_hours1').val() == 'opening'){
+             //console.log(34);
             deleteMarkers();
             document.getElementById('sidebar-left').innerHTML = "";
-            var working=[];
+            var Opening=[];
 
             for(var i=0;i<data.length;i++){
-              if(day!=data[i].WorkingExcp&&day>=data[i].WorkingWeek[0]&&day<=data[i].WorkingWeek[1]){
-                 
-                 if(data[i].WorkingTime.length==2){
-                    if(data[i].WorkingTime[1]>now>data[i].WorkingTime[0]){
-                      working.push(data[i]);
+              if(day!=data[i].WorkingExcp&&data[i].WorkingWeek[1]>=day>=data[i].WorkingWeek[0]){
+                  switch(data[i].WorkingTime.length){
+                    case 2:
+                      if(data[i].WorkingTime[0]<=nowtime&&nowtime<=data[i].WorkingTime[1]){
+                       Opening.push(data[i]);
+                      }
+                      break;
 
-                    }else{
-                      console.log(1);
-                    }
-                 }else if(data[i].WorkingTime.length==4){
-                    if(data[i].WorkingTime[1]>now>data[i].WorkingTime[0]||data[i].WorkingTime[3]>now>data[i].WorkingTime[2]){
-                      working.push(data[i]);
-                      // alert(data[i].WorkingTime[1]+">="+now+">="+data[i].WorkingTime[0]);
-                    }else{
-                      console.log(2);
-                    }
-                 }else if(data[i].WorkingTime.length==6){
-                    if(data[i].WorkingTime[1]>now>data[i].WorkingTime[0]||data[i].WorkingTime[3]>now>data[i].WorkingTime[2]||data[i].WorkingTime[5]>now>data[i].WorkingTime[4]){
-                      working.push(data[i]);
+                    case 4:
+                      if(data[i].WorkingTime[0]<=nowtime&&nowtime<=data[i].WorkingTime[1]){
+                        Opening.push(data[i]);
+                      }else if(data[i].WorkingTime[2]<=nowtime&&nowtime<=data[i].WorkingTime[3]){
+                        Opening.push(data[i]);
+                      }
+                      break;
 
-                    }else{
-                      console.log(3);
-                    }
-                 }else if(data[i].WorkingTime.length==8){
-                     if(data[i].WorkingTime[1]>now>data[i].WorkingTime[0]||data[i].WorkingTime[3]>now>data[i].WorkingTime[2]||data[i].WorkingTime[5]>now>data[i].WorkingTime[4]||data[i].WorkingTime[7]>now>data[i].WorkingTime[6]){
-                      working.push(data[i]);
+                    case 6:
+                       if(data[i].WorkingTime[0]<=nowtime&&nowtime<=data[i].WorkingTime[1]){
+                        Opening.push(data[i]);
+                      }else if(data[i].WorkingTime[2]<=nowtime&&nowtime<=data[i].WorkingTime[3]){
+                        Opening.push(data[i]);
+                      }else if(data[i].WorkingTime[4]<=nowtime&&nowtime<=data[i].WorkingTime[5]){
+                        Opening.push(data[i]);
+                      }
+                      break;
 
-                    }else{
-                      console.log(4);
-                    }
-                 }else{
-                  console.log(5);
-                 }
-              }else{
-                console.log(6);
+                    case 8:
+                      if(data[i].WorkingTime[0]<=nowtime&&nowtime<=data[i].WorkingTime[1]){
+                        Opening.push(data[i]);
+                      }else if(data[i].WorkingTime[2]<=nowtime&&nowtime<=data[i].WorkingTime[3]){
+                        Opening.push(data[i]);
+                      }else if(data[i].WorkingTime[4]<=nowtime&&nowtime<=data[i].WorkingTime[5]){
+                        Opening.push(data[i]);
+                      }else if(data[i].WorkingTime[6]<=nowtime&&nowtime<=data[i].WorkingTime[7]){
+                        Opening.push(data[i]);
+                      break;
+                      }
+                  }
               }
             }
 
-            show_Data(working);
+            show_Data(Opening);
         }else{
 
         }
@@ -359,6 +378,14 @@ var dt = new Date();
   function show_Favorite(){
     var show_Favorite=[]
 
+    document.getElementById('filter').innerHTML = "";
+    $('#filter').append(
+      '營業時間：<select id="opening_hours2">'+
+      '<option value="all" selected="selected">不限</option>'+
+      '<option value="opening">現在營業</option>'+
+      '</select>'
+    );
+
     deleteMarkers();
     document.getElementById('sidebar-left').innerHTML = "";
         
@@ -374,48 +401,71 @@ var dt = new Date();
       map.panTo(center);
       map.setZoom(10);
       show_Data(show_Favorite);
+
+      $('#opening_hours2').change(function(){
+
+          if ( $('#opening_hours2').val() == 'all' ) {
+              console.log(12);
+              deleteMarkers();
+              document.getElementById('sidebar-left').innerHTML = "";
+              show_Data(show_Favorite);
+          }else if($('#opening_hours2').val() == 'opening'){
+              deleteMarkers();
+              document.getElementById('sidebar-left').innerHTML = "";
+
+              var Opening=[];
+
+              for(var i=0;i<show_Favorite.length;i++){
+                if(day!=show_Favorite[i].WorkingExcp&&show_Favorite[i].WorkingWeek[1]>=day>=show_Favorite[i].WorkingWeek[0]){
+                    switch(show_Favorite[i].WorkingTime.length){
+                      case 2:
+                        if(show_Favorite[i].WorkingTime[0]<=nowtime&&nowtime<=show_Favorite[i].WorkingTime[1]){
+                         Opening.push(show_Favorite[i]);
+                        }
+                        break;
+
+                      case 4:
+                        if(show_Favorite[i].WorkingTime[0]<=nowtime&&nowtime<=show_Favorite[i].WorkingTime[1]){
+                          Opening.push(show_Favorite[i]);
+                        }else if(show_Favorite[i].WorkingTime[2]<=nowtime&&nowtime<=show_Favorite[i].WorkingTime[3]){
+                          Opening.push(show_Favorite[i]);
+                        }
+                        break;
+
+                      case 6:
+                         if(show_Favorite[i].WorkingTime[0]<=nowtime&&nowtime<=show_Favorite[i].WorkingTime[1]){
+                          Opening.push(show_Favorite[i]);
+                        }else if(show_Favorite[i].WorkingTime[2]<=nowtime&&nowtime<=show_Favorite[i].WorkingTime[3]){
+                          Opening.push(show_Favorite[i]);
+                        }else if(show_Favorite[i].WorkingTime[4]<=nowtime&&nowtime<=show_Favorite[i].WorkingTime[5]){
+                          Opening.push(show_Favorite[i]);
+                        }
+                        break;
+
+                      case 8:
+                        if(show_Favorite[i].WorkingTime[0]<=nowtime&&nowtime<=show_Favorite[i].WorkingTime[1]){
+                          Opening.push(show_Favorite[i]);
+                        }else if(show_Favorite[i].WorkingTime[2]<=nowtime&&nowtime<=show_Favorite[i].WorkingTime[3]){
+                          Opening.push(show_Favorite[i]);
+                        }else if(show_Favorite[i].WorkingTime[4]<=nowtime&&nowtime<=show_Favorite[i].WorkingTime[5]){
+                          Opening.push(show_Favorite[i]);
+                        }else if(show_Favorite[i].WorkingTime[6]<=nowtime&&nowtime<=show_Favorite[i].WorkingTime[7]){
+                          Opening.push(show_Favorite[i]);
+                        break;
+                        }
+                    }
+                }
+              }
+
+              show_Data(Opening);
+          }else{
+
+          }
+
+      });
       
-
     });
-  }
-  function opening_hour(){
 
-  }
-
-  function working(){
-    var working=[]
-    deleteMarkers();
-    document.getElementById('sidebar-left').innerHTML = ""; 
-    // var day=date.getDay();
-    var date=new Date();
-    var day=(date.getDay()+1);
-    var hour=(date.getHours());
-    var minute=(date.getMinutes());
-
-    $.get( "/list", function( data ) {
-      for(var i=0;i<data.length;i++){
-        var format=(data[i].營業時間)
-      .replace(/週/g,"")
-      .replace(/一/g,"1")
-      .replace(/二/g,"2")
-      .replace(/三/g,"3")
-      .replace(/四/g,"4")
-      .replace(/五/g,"5")
-      .replace(/六/g,"6")
-      .replace(/日/g,"7");
-      
-      if(format.indexOf('/')==-1){
-       if(day>=format[0]&&day<=format[2]){
-          working.push(data[i]);
-       }
-      }
-
-      }
-      show_Data(working);
-      var center = { lat: 23.099533, lng: 120.203401 };
-      map.panTo(center);
-      map.setZoom(10);
-    });
   }
   
   function setMapOnAll(map) {
