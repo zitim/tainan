e0d63d3c0932d0dc80e3c63262ae72e5bc6842ab
zitim <s911297@gmail.com>
2016-12-31 14:18:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var partials = require('express-partials');
 var static = require('serve-static');
 var bodyParser = require('body-parser');
 var favicon = require('serve-favicon'); 
-
+var session = require('express-session');
 
 // router設定
 var page = require('./routes/page');
@@ -18,12 +18,16 @@ app.set('port', (process.env.PORT || 3001));
 // 讓回傳的值可以解析 json與 urlencoded
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true}));
-
+app.use(session({
+  secret: 'recommand 128 bytes random string', // 建议使用 128 个字符的随机字符串
+}));
 
 // 版型設定
 app.use(partials());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use("/public/assets/js/markerclusterer.js", express.static(__dirname + '/public/assets/js/markerclusterer.js'));
+app.use("/public/assets/img", express.static(__dirname + '/public/assets/img'));
 
 //設定預設指定目錄
 app.use( static( path.join( __dirname, 'public' )));
@@ -41,11 +45,7 @@ app.post('/postAjax',page.postAjax);
 app.get('/getAjax',page.getAjax);
 app.get('/getJson', page.getJson);
 app.get('/list',page.list);
-app.use("/public/assets/js/markerclusterer.js", express.static(__dirname + '/public/assets/js/markerclusterer.js'));
-app.use("/public/assets/img", express.static(__dirname + '/public/assets/img'));
-
-//偵測3000 port
-
+app.post('/delete_session',page.delete_session);
 //偵測3000 port
 
 app.listen(app.get('port'), function() {
