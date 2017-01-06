7c3f00b2030556477ae82520c90d2350f3ce9900
zitim <s911297@gmail.com>
2017-01-06 18:03:51 +0800

    Update googlemaps_res.js

--- public/assets/js/googlemaps_res.js
+++ public/assets/js/googlemaps_res.js
@@ -11,6 +11,7 @@ var day=date.getDay();
 var hour=date.getHours();
 var minute=date.getMinutes();
 var nowtime=(hour*60)+minute;
+var user_id=67;
 // nowtime=60;
 //console.log(nowtime);
   
@@ -589,7 +590,6 @@ var nowtime=(hour*60)+minute;
     document.getElementById('sidebar-left').innerHTML = "";
 
     $("#opening_hours2").change(function(){
-      document.getElementById("loader").style.display = "";
       $( "#opening_hours2 option:selected" ).filter(function() {
         switch($('#opening_hours2').val()){
           case 'all':
@@ -901,8 +901,8 @@ var nowtime=(hour*60)+minute;
     marker.setMap(map);
     infoWindow.open(map, marker);
     focusInfoWindow = infoWindow;
-    map.panTo(22.883156, 119.905706);
-    map.setZoom(15);
+    map.panTo(marker.getPosition());
+    map.setZoom(12);
 
     }).trigger( "change" );
 
